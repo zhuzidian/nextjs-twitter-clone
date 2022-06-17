509a536793cb0a96af70bdc6d0f9061a0613e021
ZHU Zidian <zhuzidian@gmail.com>
2022-06-17 20:32:27 +0900

    update feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,14 @@
+import { HiOutlineSparkles } from "react-icons/hi"
+
 export default function Feed(props) {
   return (
     <div className="sm:ml-[73px] xl:ml-[370px] border-l border-r border-gray-300 flex-grow max-w-2xl">
-      Feed
+      <div className="flex items-center justify-between py-2 px-3 sticky top-0 bg-white border-b border-gray-300">
+        <h2 className="text-lg sm:text-xl font-bold">最新のツイート</h2>
+        <div className="hover-animation w-9 h-9 flex items-center justify-center xl:px-0">
+          <HiOutlineSparkles className="h-5" />
+        </div>
+      </div>
     </div>
   )
 }
